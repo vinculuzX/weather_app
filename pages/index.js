ebc652867fc68581eec8d62fe69cab2d04b469ea
vinculuzX <vinculuzX.app@gmail.com>
2022-03-04 17:23:08 -0300

    change weather page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Layout from '../components/layout'
-import WheatherPage from './weather'
+import WeatherPage from './weather'
 
 export default function Home() {
   return (
     <Layout>
       <h1 className='text-3xl'>Wheather App</h1>
       <div>
-        <WheatherPage />
+        <WeatherPage />
       </div>
     </Layout>
   )
